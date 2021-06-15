227e4fd3183e013fdcbbdb8ad0e795ab2eddc7ba
Son Chau <soncqb@gmail.com>
2021-06-15 15:59:57 -0500

    Update singly_linked_list.py

--- Singly Linked List/singly_linked_list.py
+++ Singly Linked List/singly_linked_list.py
@@ -5,3 +5,33 @@ class Node:
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+
+    def append(self, data) -> None:
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+    
+    def print_list(self) -> None:
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+    
+    def prepend(self, data) -> None:
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+
+llist.prepend("D")
+
+llist.print_list()  
