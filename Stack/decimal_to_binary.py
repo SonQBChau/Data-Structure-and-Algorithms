568985a6c25520a2a640e688d712876702bcd636
Son Chau <soncqb@gmail.com>
2021-06-14 22:51:52 -0500

    Singly Linked List

--- Stack/decimal_to_binary.py
+++ Stack/decimal_to_binary.py
@@ -3,7 +3,7 @@ from stack import Stack
 def convert_int_to_bin(dec_num : int) -> str:
     if dec_num == 0:
         return 0
-        
+
     curr_num = dec_num
     s = Stack()
     binary = ''
@@ -18,3 +18,8 @@ def convert_int_to_bin(dec_num : int) -> str:
     return binary
 
 print(convert_int_to_bin(242))
+print(convert_int_to_bin(56))
+print(convert_int_to_bin(2))
+print(convert_int_to_bin(32))
+print(convert_int_to_bin(10))
+print(int(convert_int_to_bin(56),2)==56)
