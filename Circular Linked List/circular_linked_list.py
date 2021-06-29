94feaae61b2fe48ede9d5065ab838d9cbdf7a31a
Son Chau <soncqb@gmail.com>
2021-06-28 20:01:21 -0500

    Update circular_linked_list.py

--- Circular Linked List/circular_linked_list.py
+++ Circular Linked List/circular_linked_list.py
@@ -11,7 +11,17 @@ class CircularLinkedList:
         pass
 
     def append(self, data):
-        pass
+        # if empty, we create a head node first
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else: 
+            new_data = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_data
+            new_data.next = self.head
 
     def print_list(self):
         pass
