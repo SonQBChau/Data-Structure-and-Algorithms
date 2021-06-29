8246f2ebdd72c46fa2b40d65c0c057cf82a0353f
Son Chau <soncqb@gmail.com>
2021-06-29 13:46:55 -0500

    Create josephus_circle.py

---
+++ Circular Linked List/josephus_circle.py
@@ -0,0 +1,12 @@
+from circular_linked_list import CircularLinkedList
+def josephus_circle(cllist, step):
+    pass
+
+cllist = CircularLinkedList()
+cllist.append(1)
+cllist.append(2)
+cllist.append(3)
+cllist.append(4)
+
+
+josephus_circle(cllist, 2)
