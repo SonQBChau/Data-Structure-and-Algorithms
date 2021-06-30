9ebd55564ccb92edeeced4214b394ed4e5287dec
Son Chau <soncqb@gmail.com>
2021-06-30 17:14:54 -0500

    is circular linked list

--- Circular Linked List/is_circular_linked_list.py
+++ Circular Linked List/is_circular_linked_list.py
@@ -1,4 +1,5 @@
 from circular_linked_list import CircularLinkedList
+from singly_linked_list import LinkedList
 
 def is_circular_linked_list(input_list):
     if input_list.head:
@@ -7,7 +8,21 @@ def is_circular_linked_list(input_list):
             cur = cur.next
             if cur.next == input_list.head:
                 return True
-            return False
+        return False
     else:
         return False
 
+cllist = CircularLinkedList()
+cllist.append(1)
+cllist.append(2)
+cllist.append(3)
+cllist.append(4)
+
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+
+print(is_circular_linked_list(cllist))
+print(is_circular_linked_list(llist))
