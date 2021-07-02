3df4da01fff258eb2519fba03d24734e49464326
Son Chau <soncqb@gmail.com>
2021-07-02 13:45:29 -0500

    Update doubly_linked_list.py

--- Doubly Linked List/doubly_linked_list.py
+++ Doubly Linked List/doubly_linked_list.py
@@ -22,7 +22,15 @@ class DoublyLinkedList:
 
 
     def prepend(self, data):
-        pass
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            new_node = Node(data)
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            
 
     def print_list(self):
         cur = self.head
