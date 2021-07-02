ce89b2afcf5d4005bce144502b76aa41040c6577
Son Chau <soncqb@gmail.com>
2021-07-02 13:20:33 -0500

    Create doubly_linked_list.py

---
+++ Doubly Linked List/doubly_linked_list.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        pass
+
+    def prepend(self, data):
+        pass
+
+    def print_list(self):
+        pass 
