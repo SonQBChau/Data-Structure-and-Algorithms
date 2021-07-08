8f66e42c6aa81f2dcf256148fdcd71d8c7b65022
Son Chau <soncqb@gmail.com>
2021-07-07 22:48:05 -0500

    Update two_sum_problem.py

--- Arrays/two_sum_problem.py
+++ Arrays/two_sum_problem.py
@@ -20,10 +20,27 @@ def two_sum_hash_table(A, target):
             ht[target-A[i]] = A[i]
     return False
 
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def two_sum(A, target):
+    i = 0
+    j = len(A) - 1
+    while i < j:
+        if A[i] + A[j] == target:
+            print(A[i], A[j])
+            return True
+        elif A[i] + A[j] < target:
+            i += 1
+        else:
+            j -= 1
+    return False
+
 A = [-2, 1, 2, 4, 7, 11]
 target = 13
 print(two_sum_brute_force(A,target))
 print(two_sum_hash_table(A,target))
+print(two_sum(A,target))
 target = 20
 print(two_sum_brute_force(A,target))
 print(two_sum_hash_table(A,target))
+print(two_sum(A,target))
