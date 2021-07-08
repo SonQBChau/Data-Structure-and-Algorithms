b9adcb0efc4b8b3cefb3b6d459e3608c8a50cdb3
Son Chau <soncqb@gmail.com>
2021-07-07 22:42:26 -0500

    Update two_sum_problem.py

--- Arrays/two_sum_problem.py
+++ Arrays/two_sum_problem.py
@@ -8,8 +8,22 @@ def two_sum_brute_force(A, target):
                 return True
     return False
 
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def two_sum_hash_table(A, target):
+    ht = dict()
+    for i in range(len(A)):
+        if A[i] in ht:
+            print(ht[A[i]], A[i])
+            return True
+        else:
+            ht[target-A[i]] = A[i]
+    return False
+
 A = [-2, 1, 2, 4, 7, 11]
 target = 13
 print(two_sum_brute_force(A,target))
+print(two_sum_hash_table(A,target))
 target = 20
 print(two_sum_brute_force(A,target))
+print(two_sum_hash_table(A,target))
