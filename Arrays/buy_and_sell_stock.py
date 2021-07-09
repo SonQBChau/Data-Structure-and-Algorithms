dcca098d69d60736f97bcc93fea21c6822b27313
Son Chau <soncqb@gmail.com>
2021-07-08 19:42:18 -0500

    Update buy_and_sell_stock.py

--- Arrays/buy_and_sell_stock.py
+++ Arrays/buy_and_sell_stock.py
@@ -1,2 +1,12 @@
-def buy_and_sell_stock_once(prices):
-  pass
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+def buy_and_sell_once(A):
+  max_profit = 0
+  for i in range(len(A)-1):
+    for j in range(i+1, len(A)):
+      if A[j] - A[i] > max_profit:
+          max_profit = A[j] - A[i]
+  return max_profit
+
+A = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+print(buy_and_sell_once(A))
