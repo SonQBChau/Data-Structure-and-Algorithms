594f059e91e450a64ebebd5c3fb655144385df8c
Son Chau <soncqb@gmail.com>
2021-07-15 18:01:46 -0500

    Create binary_search_trees.py

---
+++ Binary Search Trees/binary_search_trees.py
@@ -0,0 +1,11 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+        
