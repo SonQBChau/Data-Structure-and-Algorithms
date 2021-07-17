d53973c0143e7758f49e8d57c0a9e670230bb428
Son Chau <soncqb@gmail.com>
2021-07-17 16:09:52 -0500

    Update binary_search_trees.py

--- Binary Search Trees/binary_search_trees.py
+++ Binary Search Trees/binary_search_trees.py
@@ -4,8 +4,14 @@ class Node(object):
         self.left = None
         self.right = None
 
+
 class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
-        
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
+
+
+    def insert_helper(self, current, new_val):
