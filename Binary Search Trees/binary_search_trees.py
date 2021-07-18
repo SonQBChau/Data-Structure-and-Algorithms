785fe4f3ca5e42367f55e97a608d207a65c8eb0b
Son Chau <soncqb@gmail.com>
2021-07-17 22:24:34 -0500

    Update binary_search_trees.py

--- Binary Search Trees/binary_search_trees.py
+++ Binary Search Trees/binary_search_trees.py
@@ -36,6 +36,24 @@ class BST(object):
             else:
                 return self.search_helper(current.left, find_val)
 
+    def is_bst_satisfied(self):
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            
+            val = node.data
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(self.root)
+
+
 
 bst = BST(10)
 bst.insert(3)
