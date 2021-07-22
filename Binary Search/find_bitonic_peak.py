afd521ccddf8741182383e60e73d4c87d30866b2
Son Chau <soncqb@gmail.com>
2021-07-21 22:14:43 -0500

    Update find_bitonic_peak.py

--- Binary Search/find_bitonic_peak.py
+++ Binary Search/find_bitonic_peak.py
@@ -1,32 +1,32 @@
-def find_highest_number_naive(A):
-    peak = A[0]
-    peak_pos = 0
-    for i in range(len(A)):
-        if A[i] > peak:
-            peak = A[i]
-            peak_pos = i
-    if peak_pos == 0 or peak_pos == len(A)-1:
-        return None
-    if A[peak_pos-1] > A[peak_pos] and A[peak_pos] < A[peak_pos+1]:
-        return None
-
-    return A[peak_pos]
-
-    
-
 def find_highest_number(A):
-    pass
+    low = 0
+    high = len(A) - 1
+
+    # Require at least 3 elements for a bitonic sequence.
+    if len(A) < 3:
+        return None
+
+    while low <= high:
+        mid = (low + high)//2
+
+        mid_left = A[mid - 1] if mid - 1 >= 0 else float("-inf")
+        mid_right = A[mid + 1] if mid + 1 < len(A) else float("inf")
+
+        if mid_left < A[mid] and mid_right > A[mid]:
+            low = mid + 1
+        elif mid_left > A[mid] and mid_right < A[mid]:
+            high = mid - 1
+        elif mid_left < A[mid] and mid_right < A[mid]:
+            return A[mid]
+    return None
+
 
 # Peak element is "5".
 A = [1, 2, 3, 4, 5, 4, 3, 2, 1]
 print(find_highest_number(A))
-print(find_highest_number_naive(A))
 A = [1, 6, 5, 4, 3, 2, 1]
 print(find_highest_number(A))
-print(find_highest_number_naive(A))
 A = [1, 2, 3, 4, 5]
 print(find_highest_number(A))
-print(find_highest_number_naive(A))
 A = [5, 4, 3, 2, 1]
 print(find_highest_number(A))
-print(find_highest_number_naive(A))
