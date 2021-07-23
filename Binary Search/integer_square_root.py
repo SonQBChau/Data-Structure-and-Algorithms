2371e759d5a867787311c42124f324747aea1e6a
Son Chau <soncqb@gmail.com>
2021-07-23 17:11:00 -0500

    Update integer_square_root.py

--- Binary Search/integer_square_root.py
+++ Binary Search/integer_square_root.py
@@ -9,18 +9,31 @@ def integer_square_root_naive(k):
 
 
 def integer_square_root(k):
-    return 0
+    low = 0
+    high = k
+    while low <= high:
+        mid = (low+high) // 2
+        mid_squared = mid*mid
+        if mid_squared <= k:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low - 1
 
 
 x = integer_square_root_naive(300)  # 17
 y = integer_square_root(300)
 print(x)
+print(y)
 x = integer_square_root_naive(12)  # 3
 y = integer_square_root(12)
 print(x)
+print(y)
 x = integer_square_root_naive(1000)  # 31
 y = integer_square_root(1000)
 print(x)
+print(y)
 x = integer_square_root_naive(625)  # 25
 y = integer_square_root(625)
 print(x)
+print(y)
