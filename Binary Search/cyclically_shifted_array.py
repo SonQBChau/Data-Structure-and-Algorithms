f55b945c605767f2cf7066da2aae51d08e80a186
Son Chau <soncqb@gmail.com>
2021-07-23 17:24:36 -0500

    Update cyclically_shifted_array.py

--- Binary Search/cyclically_shifted_array.py
+++ Binary Search/cyclically_shifted_array.py
@@ -1,11 +1,23 @@
-def find(A):
-  pass
+def find_naive(A):
+    lowest_idx = 0
+    for i in range(len(A)):
+        if A[i] < A[lowest_idx]:
+            lowest_idx = i
+    return lowest_idx
+  
 
 
-find([4, 5, 6, 7, 1, 2, 3]) #4
-find([6, 7, 1, 2, 3, 4, 5]) #2
-find([7, 1, 2, 3, 4, 5, 6]) #1
-find([1, 2, 3, 4, 5, 6, 7]) #0
-find([3, 4, 5, 6, 7, 1, 2]) #5
-find([2, 3, 4, 5, 6, 7, 1]) #6
-find([5, 6, 7, 1, 2, 3, 5]) #3
+x = find_naive([4, 5, 6, 7, 1, 2, 3]) #4
+print(x)
+x = find_naive([6, 7, 1, 2, 3, 4, 5])  # 2
+print(x)
+x = find_naive([7, 1, 2, 3, 4, 5, 6])  # 1
+print(x)
+x = find_naive([1, 2, 3, 4, 5, 6, 7])  # 0
+print(x)
+x = find_naive([3, 4, 5, 6, 7, 1, 2])  # 5
+print(x)
+x = find_naive([2, 3, 4, 5, 6, 7, 1])  # 6
+print(x)
+x = find_naive([5, 6, 7, 1, 2, 3, 5])  # 3
+print(x)
