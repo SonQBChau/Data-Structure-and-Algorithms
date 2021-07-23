bd4e9468d1e21a15004cde5d168d1720d7b73f73
Son Chau <soncqb@gmail.com>
2021-07-23 10:26:14 -0500

    Update find_first_entry.py

--- Binary Search/find_first_entry.py
+++ Binary Search/find_first_entry.py
@@ -1,11 +1,31 @@
-def find(A, target):
+def find_naive(A, target):
     for i in range(len(A)):
         if A[i] == target:
             return i
     return None
 
 
+def find(A, target):
+    low = 0
+    high = len(A)-1
+    while low <= high:
+        mid = (low+high) // 2
+        if A[mid] > target:
+            high = mid - 1  
+        elif A[mid] < target:
+            low = mid + 1
+        else:
+            if mid-1 < 0:
+                return mid
+            elif A[mid-1] != target:
+                return mid
+            else:
+                high = mid-1
+    return None
+
 A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
 target = 108
-x = find(A, target)
+x = find_naive(A, target)
 print(x)
+y = find(A, target)
+print(y)
