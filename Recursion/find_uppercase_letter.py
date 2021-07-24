8b4fe7da6b1f24cfc85b0a3cefc1d2dd752d8c1a
Son Chau <soncqb@gmail.com>
2021-07-24 16:16:31 -0500

    Update find_uppercase_letter.py

--- Recursion/find_uppercase_letter.py
+++ Recursion/find_uppercase_letter.py
@@ -1,11 +1,32 @@
 def find_uppercase_iterative(input_str):
+    for i in range(len(input_str)):
+        if input_str[i].isupper():
+            return input_str[i]
     
     return "No uppercase character found"
 
 
+def find_uppercase_recursive(input_str, idx=0):
+    if idx >= len(input_str):
+        return "No uppercase character found"
+    if input_str[idx].isupper():
+        return input_str[idx]
+    else:
+        idx += 1
+        return find_uppercase_recursive(input_str, idx)
+
 str_1 = "lucidProgramming"
 str_2 = "LucidProgramming"
 str_3 = "lucidprogramming"
-find_uppercase_iterative(str_1)
-find_uppercase_iterative(str_2)
-find_uppercase_iterative(str_3)
+x = find_uppercase_iterative(str_1)
+print(x)
+x = find_uppercase_iterative(str_2)
+print(x)
+x = find_uppercase_iterative(str_3)
+print(x)
+x = find_uppercase_recursive(str_1)
+print(x)
+x = find_uppercase_recursive(str_2)
+print(x)
+x = find_uppercase_recursive(str_3)
+print(x)
