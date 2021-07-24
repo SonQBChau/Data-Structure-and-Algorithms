6a16c9346c7baddeb58c43b3323d1e5882ea24ce
Son Chau <soncqb@gmail.com>
2021-07-24 16:30:25 -0500

    Update count_consonants.py

--- Recursion/count_consonants.py
+++ Recursion/count_consonants.py
@@ -2,7 +2,11 @@ vowels = "aeiou"
 
 
 def iterative_count_consonants(input_str):
-    pass
+    count = 0
+    for i in range(len(input_str)):
+        if input_str[i].lower() not in vowels and input_str[i].isalpha():
+            count += 1
+    return count
 
 
 def recursive_count_consonants(input_str):
@@ -11,6 +15,8 @@ def recursive_count_consonants(input_str):
 input_str = "abc de"
 print(input_str)
 print(iterative_count_consonants(input_str))
+print(recursive_count_consonants(input_str))
 input_str = "LuCiDPrograMMiNG"
 print(input_str)
+print(iterative_count_consonants(input_str))
 print(recursive_count_consonants(input_str))
