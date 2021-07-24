425f6124e028f65acddb44af768147fd40da789e
Son Chau <soncqb@gmail.com>
2021-07-24 16:25:55 -0500

    Create count_consonants.py

---
+++ Recursion/count_consonants.py
@@ -0,0 +1,16 @@
+vowels = "aeiou"
+
+
+def iterative_count_consonants(input_str):
+    pass
+
+
+def recursive_count_consonants(input_str):
+    pass
+
+input_str = "abc de"
+print(input_str)
+print(iterative_count_consonants(input_str))
+input_str = "LuCiDPrograMMiNG"
+print(input_str)
+print(recursive_count_consonants(input_str))
