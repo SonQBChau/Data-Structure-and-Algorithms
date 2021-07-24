a911affda7b54cefcf1c462de594bd1623e5347b
Son Chau <soncqb@gmail.com>
2021-07-24 16:34:05 -0500

    Update count_consonants.py

--- Recursion/count_consonants.py
+++ Recursion/count_consonants.py
@@ -10,7 +10,13 @@ def iterative_count_consonants(input_str):
 
 
 def recursive_count_consonants(input_str):
-    pass
+    if input_str == '':
+        return 0
+    if input_str[0].lower() not in vowels and input_str[0].isalpha():
+        return 1 + recursive_count_consonants(input_str[1:])
+    else:
+        return recursive_count_consonants(input_str[1:])
+    
 
 input_str = "abc de"
 print(input_str)
