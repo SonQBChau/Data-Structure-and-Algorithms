b3f180158f43a9738a7a3c76e35b38913a628374
Son Chau <soncqb@gmail.com>
2021-07-24 16:03:24 -0500

    Create find_uppercase_letter.py

---
+++ Recursion/find_uppercase_letter.py
@@ -0,0 +1,11 @@
+def find_uppercase_iterative(input_str):
+    
+    return "No uppercase character found"
+
+
+str_1 = "lucidProgramming"
+str_2 = "LucidProgramming"
+str_3 = "lucidprogramming"
+find_uppercase_iterative(str_1)
+find_uppercase_iterative(str_2)
+find_uppercase_iterative(str_3)
