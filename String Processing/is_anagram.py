f2ad0580f0223cdf0949262d17588a4885f9c424
Son Chau <soncqb@gmail.com>
2021-08-05 22:56:40 -0500

    Create is_anagram.py

---
+++ String Processing/is_anagram.py
@@ -0,0 +1,11 @@
+
+s1 = "fairy tales"
+s2 = "rail safety"
+
+s1 = s1.replace(" ", "").lower()
+s2 = s2.replace(" ", "").lower()
+
+# Requires n log n time (since any comparison 
+# based sorting algorithm requires at least 
+# n log n time to sort).
+print(sorted(s1) == sorted(s2))
