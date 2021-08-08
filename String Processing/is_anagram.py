d12febe320e4ee4e753fc7dbf9db17a200733ae2
Son Chau <soncqb@gmail.com>
2021-08-08 09:54:49 -0500

    Update is_anagram.py

--- String Processing/is_anagram.py
+++ String Processing/is_anagram.py
@@ -9,3 +9,30 @@ s2 = s2.replace(" ", "").lower()
 # based sorting algorithm requires at least 
 # n log n time to sort).
 print(sorted(s1) == sorted(s2))
+
+def is_anagram(s1, s2):
+    ht = dict()
+    
+    if len(s1) != len(s2):
+        return False
+    
+    for i in s1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+            
+    for i in s2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+            
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    
+    return True
+
+
+print(is_anagram(s1, s2))
