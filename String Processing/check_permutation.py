0aebc7bec43572fb6e819fae5d9eae4bbd0e940a
Son Chau <soncqb@gmail.com>
2021-08-10 17:38:22 -0500

    Update check_permutation.py

--- String Processing/check_permutation.py
+++ String Processing/check_permutation.py
@@ -4,20 +4,20 @@
 def is_perm_1(str_1, str_2):
     str_1 = str_1.lower()
     str_2 = str_2.lower()
-    
+
     if len(str_1) != len(str_2):
         return False
-    
+
     str_1 = ''.join(sorted(str_1))
     str_2 = ''.join(sorted(str_2))
-    
+
     n = len(str_1)
-    
+
     for i in range(n):
         if str_1[i] != str_2[i]:
             return False
-    
     return True
+    
 
 # Approach 2: Hash Table
 # Time Complexity: O(n)
@@ -25,23 +25,32 @@ def is_perm_1(str_1, str_2):
 def is_perm_2(str_1, str_2):
     str_1 = str_1.lower()
     str_2 = str_2.lower()
-    
+
     if len(str_1) != len(str_2):
         return False
 
     d = dict()
     
     for i in str_1:
-        if i  in d:
+        if i in d:
             d[i] += 1
         else:
             d[i] = 1
-    
     for i in str_2:
         if i in d:
             d[i] -= 1
         else:
             return False
-    
-    
+
     return all(value == 0 for value in d.values())
+
+is_permutation_1 = "google"
+is_permutation_2 = "ooggle"
+
+not_permutation_1 = "not"
+not_permutation_2 = "top"
+print(is_perm_1(is_permutation_1, is_permutation_2))
+print(is_perm_1(not_permutation_1, not_permutation_2))
+
+print(is_perm_2(is_permutation_1, is_permutation_2))
+print(is_perm_2(not_permutation_1, not_permutation_2))
