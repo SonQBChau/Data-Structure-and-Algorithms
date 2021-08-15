d4ead51e899f40d5c698db7c0a466e9362c7ba47
Son Chau <soncqb@gmail.com>
2021-08-15 15:51:07 -0500

    Update string_to_integer.py

--- String Processing/string_to_integer.py
+++ String Processing/string_to_integer.py
@@ -1,13 +1,32 @@
 def str_to_int(input_str):
-    
-    return 0
+
+    output_int = 0
+
+    if input_str[0] == '-':
+        start_idx = 1
+        is_negative = True
+    else:
+        start_idx = 0
+        is_negative = False
+
+    for i in range(start_idx, len(input_str)):
+        place = 10**(len(input_str) - (i+1))
+        digit = ord(input_str[i]) - ord('0')
+        output_int += place * digit
+
+    if is_negative:
+        return -1 * output_int
+    else:
+        return output_int
 
 
 
-output_str = '123'
-print(output_str)
-print(type(output_str))
+s = "554"
+x = str_to_int(s)
+print(type(x))
 
-input_int = str_to_int(output_str)
-print(input_int)
-print(type(input_int))
+s = "123"
+print(str_to_int(s))
+
+s = "-123"
+print(str_to_int(s))
